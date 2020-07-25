f78bc9d33a8839a6605b9b7bb240e692d3a33d72
Srikanth <kvsrikanth.kvs@gmail.com>
2020-07-25 13:14:53 +0530

    commit 1

---
+++ project/index.js
@@ -0,0 +1,57 @@
+var buttonscount = document.querySelectorAll(".drum").length;
+for (var i = 0; i < buttonscount; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener("click", function() {
+        var buttontext = this.innerText;
+        KeyToSound(buttontext);
+        boxClick(buttontext);
+    })
+}
+
+document.addEventListener('keydown', function(event) {
+    var key = event.key;
+    KeyToSound(key);
+    boxClick(key);
+});
+
+function KeyToSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+    }
+}
+
+function boxClick(key) {
+
+    document.querySelector('.' + key).classList.add('pressed');
+
+    setTimeout(function() {
+        document.querySelector('.' + key).classList.remove('pressed');
+    }, 100);
+}
